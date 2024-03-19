7cb0b8cc43430f4c0ac91e0f1ad1f7e1ad590bb6
Chetan3327 <chauhanchetan12789@gmail.com>
2024-03-20 01:31:17 +0530

    implemented redis

--- server/src/routes/submissions.routes.js
+++ server/src/routes/submissions.routes.js
@@ -4,9 +4,21 @@ const router = express.Router()
 import {PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient()
 
+console.log(process.env.REDIS_URL)
+import Redis from 'ioredis'
+const client = new Redis(process.env.REDIS_URL)
+
 router.get('/', async (req, res) => {
-    const submissions = await prisma.submission.findMany()
-    res.json(submissions)
+    const cache = await client.get('submissions')
+    if(cache){
+        // console.log(cache)
+        return res.json(JSON.parse(cache))
+    }else{
+        const submissions = await prisma.submission.findMany()
+        await client.set('submissions', JSON.stringify(submissions))
+        await client.expire('submissions', 100)
+        return res.json(submissions)
+    }
 })
 
 router.post('/', async (req, res) => {
